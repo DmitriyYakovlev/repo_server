729312b8c46dc0affb64664b27c54e71c18607ec
unknown <C:\Users\Dimasta\AppData\Roaming\The Bat!>
2014-04-14 00:21:44 +0300

    added table

--- src/com/example/diplom/DiplomUI.java
+++ src/com/example/diplom/DiplomUI.java
@@ -4,13 +4,19 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.data.Property;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.util.IndexedContainer;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Alignment;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.PasswordField;
+import com.vaadin.ui.Table;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -27,10 +33,19 @@ public class DiplomUI extends UI {
 
 	private TextField username;
 	private PasswordField password;
+	private static final String FNAME = "First Name";
+	private static final String LNAME = "Last Name";
+	private static final String COMPANY = "Company";
+	IndexedContainer contactContainer = createDummyDatasource();
+	
+	// second
+	private Table contactList = new Table();
 
 	@Override
 	protected void init(VaadinRequest request) {
-		setPaswordFields();
+		//setPaswordFields();
+		initLayout();
+		initContactList();
 	}
 
 	public void setPaswordFields(){
@@ -62,5 +77,62 @@ public class DiplomUI extends UI {
 
 	}
 	
+	private void initLayout() {
+
+		HorizontalSplitPanel splitPanel = new HorizontalSplitPanel();
+		setContent(splitPanel);
+
+		VerticalLayout leftLayout = new VerticalLayout();
+		splitPanel.addComponent(leftLayout);
+		
+		leftLayout.setSizeFull();
+		leftLayout.addComponent(contactList);
+		leftLayout.setExpandRatio(contactList, 1);
+		contactList.setSizeFull();
+
+	}	
+	
+	private void initContactList() {
+		contactList.setContainerDataSource(contactContainer);
+		contactList.setVisibleColumns(new String[] { FNAME, LNAME, COMPANY });
+		contactList.setSelectable(true);
+		contactList.setImmediate(true);
+
+		contactList.addValueChangeListener(new Property.ValueChangeListener() {
+
+			@Override
+			public void valueChange(ValueChangeEvent event) {
+				Object contactId = contactList.getValue();
+			}
+		});
+	}
+
+	private static final String[] fieldNames = new String[] { FNAME, LNAME,
+		COMPANY, "Mobile Phone", "Work Phone", "Home Phone", "Work Email",
+		"Home Email", "Street", "City", "Zip", "State", "Country" };
+
+	private static IndexedContainer createDummyDatasource() {
+		IndexedContainer ic = new IndexedContainer();
+
+		for (String p : fieldNames) {
+			ic.addContainerProperty(p, String.class, "");
+		}
+
+		String[] fnames = { "Peter", "Alice", "Joshua", "Mike", "Olivia",
+				"Nina", "Alex", "Rita", "Dan", "Umberto", "Henrik", "Rene",
+				"Lisa", "Marge" };
+		String[] lnames = { "Smith", "Gordon", "Simpson", "Brown", "Clavel",
+				"Simons", "Verne", "Scott", "Allison", "Gates", "Rowling",
+				"Barks", "Ross", "Schneider", "Tate" };
+		for (int i = 0; i < 1000; i++) {
+			Object id = ic.addItem();
+			ic.getContainerProperty(id, FNAME).setValue(
+					fnames[(int) (fnames.length * Math.random())]);
+			ic.getContainerProperty(id, LNAME).setValue(
+					lnames[(int) (lnames.length * Math.random())]);
+		}
+
+		return ic;
+	}
 	
 }
